4cb57a6baec1a3d59b017682f1f3344adb270240
tab <tukhfatullin@gmail.com>
2014-09-18 16:42:59 +0700

    Signin form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, :if => :devise_controller?
   before_action :collect_user_info
   before_action :collect_redirect_url
 
+  layout :define_layout
+
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:surname, :name, :patronymic]
     devise_parameter_sanitizer.for(:account_update) << [:surname, :name, :patronymic]
@@ -24,5 +28,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     params['redirect_url'] || super
+  end
+
+  def define_layout
+    devise_controller? ? 'session' : 'application'
   end
 end
