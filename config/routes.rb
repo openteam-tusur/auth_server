0f7b2badb759d6e425364e50cfd2315b1ca66903
Ivan Puchkov <rake.eclipse@gmail.com>
2014-09-18 09:24:29 +0700

    some sidekiq configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Server::Application.routes.draw do
+  require 'sidekiq/web'
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   devise_for :users, :controllers => { :omniauth_callbacks => 'omniauth_callbacks' }
 
   resources :users, :only => [:index] do
